c58030ef0acd7efbfa0bb0c438ea292d2d95122f
StuffyDoll187 <markeulitt@gmail.com>
2025-02-14 15:23:53 -0500

    Add files via upload

--- Bionic Overclock/OverclockSkills.cs
+++ Bionic Overclock/OverclockSkills.cs
@@ -1,7 +1,10 @@
 ﻿using Database;
 using HarmonyLib;
+using PeterHan.PLib.Core;
+using System;
 using System.Collections.Generic;
 using System.Reflection;
+using System.Reflection.Emit;
 
 namespace Bionic_Overclock
 {
@@ -16,7 +19,7 @@ namespace Bionic_Overclock
                 Db.Get().SkillGroups.Add(new SkillGroup("Overclock", null, "", "", ""));
                 Db.Get().SkillGroups.Get("Overclock").relevantAttributes = new List<Klei.AI.Attribute>();
                 Db.Get().SkillGroups.Get("Overclock").requiredChoreGroups = new List<string>();
-                Db.Get().SkillGroups.Get("Overclock").allowAsAptitude = false;
+                Db.Get().SkillGroups.Get("Overclock").allowAsAptitude = false;                
 
                 AddSkill.Invoke(__instance.Skills, new object[]
                 {
@@ -53,8 +56,60 @@ namespace Bionic_Overclock
                         priorSkills: null,
                         requiredDuplicantModel: GameTags.Minions.Models.Bionic.Name
                     )
-                });               
+                });              
             }
         }
-    }    
+
+        /// <summary>
+        /// Removes the Overclock Skill Group from the list of possible archetypes in the dropdown for starting duplicants.
+        /// Adds: 
+        ///     list.Remove(Db.Get().SkillGroups.Get("Overclock"));
+        /// just before: 
+        ///     list.Remove(Db.Get().SkillGroups.BionicSkills);
+        /// inside of CharacterContainer.OnSpawn
+        /// </summary>
+        [HarmonyPatch(typeof(CharacterContainer), "OnSpawn")]
+        public class CharacterContainer_OnSpawn_Patch_Transpiler
+        {
+            public static FieldInfo BionicSkills = AccessTools.Field(typeof(SkillGroups), "BionicSkills");
+            public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+            {
+                var codeMatcher = new CodeMatcher(instructions);
+                var codeMatch = new CodeMatch[]
+                {
+                    new CodeMatch(OpCodes.Ldloc_0),
+                    new CodeMatch(OpCodes.Call),
+                    new CodeMatch(OpCodes.Ldfld),
+                    new CodeMatch(OpCodes.Ldfld, BionicSkills),
+                    new CodeMatch(OpCodes.Callvirt),
+                    new CodeMatch(OpCodes.Pop)
+                };
+                codeMatcher.MatchStartForward(codeMatch);
+                if (codeMatcher.Remaining == 0)
+                {
+                    Debug.Log("[BionicOverclock] CharacterContainer OnSpawn Transpiler Failed, codeMatch not found");
+                    return instructions;
+                }
+
+                var resourceSet_Get_ByString_Method = typeof(ResourceSet<SkillGroup>).GetMethod(nameof(ResourceSet<SkillGroup>.Get), new Type[] { typeof(string) });
+
+                var newInstructionBlock = new CodeInstruction[] 
+                {
+                    new CodeInstruction(codeMatcher.InstructionAt(0)),
+                    new CodeInstruction(codeMatcher.InstructionAt(1)),
+                    new CodeInstruction(codeMatcher.InstructionAt(2)),
+                    new CodeInstruction(OpCodes.Ldstr, "Overclock"),
+                    new CodeInstruction(OpCodes.Callvirt, resourceSet_Get_ByString_Method),
+                    new CodeInstruction(codeMatcher.InstructionAt(4)),
+                    new CodeInstruction(codeMatcher.InstructionAt(5)) 
+                };
+
+                codeMatcher.Insert(newInstructionBlock);
+
+                Debug.Log("[BionicOverclock] CharacterContainer OnSpawn Transpiler succeeded?");
+                return codeMatcher.Instructions();
+         
+            }
+        }       
+    }
 }
