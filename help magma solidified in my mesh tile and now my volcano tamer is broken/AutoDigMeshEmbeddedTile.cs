17b56d3e46ffeb470825cbc8c8a35d92997e1a90
StuffyDoll187 <markeulitt@gmail.com>
2025-02-27 14:37:50 -0500

    Add files via upload

---
+++ help magma solidified in my mesh tile and now my volcano tamer is broken/AutoDigMeshEmbeddedTile.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace help_magma_solidified_in_my_mesh_tile_and_now_my_volcano_tamer_is_broken
+{
+    public class AutoDigMeshEmbeddedTile : KMonoBehaviour, ISim200ms
+    {                
+        int cell;
+        protected override void OnSpawn()
+        {
+            base.OnSpawn();
+            cell = Grid.PosToCell(this);
+        }
+        public void Sim200ms(float dt)
+        {
+            if (Grid.Element[cell].IsSolid)
+            {
+                SimMessages.Dig(cell);
+                //Grid.RenderedByWorld[cell] = true;
+                //World.Instance.groundRenderer.MarkDirty(cell);
+            }
+                                                     
+        }        
+    }
+    [HarmonyPatch(typeof(MeshTileConfig), nameof(MeshTileConfig.DoPostConfigureComplete))]
+    public class MeshTileConfig_DoPostConfigureComplete_Patch
+    {
+        public static void Postfix(GameObject go)
+        {
+            go.AddComponent<AutoDigMeshEmbeddedTile>();
+        }
+    }
+}
---
+++ help magma solidified in my mesh tile and now my volcano tamer is broken/Mod.cs
@@ -0,0 +1,12 @@
+using HarmonyLib;
+
+namespace help_magma_solidified_in_my_mesh_tile_and_now_my_volcano_tamer_is_broken
+{
+    public class Mod : KMod.UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+    }
+}
